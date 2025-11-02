fd55179e44c8efdef242547436b41de63080d915
Кирилл Яровой <k.yarovoy@maximaster.ru>
2025-11-02 21:04:07 +0300

    Убрал класс с конфиденциальными параметрами. Перешёл на env

--- DB/DataContext.php
+++ DB/DataContext.php
@@ -1,23 +1,25 @@
 <?php
 class PgConnect{
     private static $pgConnection;
-    private $pgPDO;
+    private PDO $pgPDO;
     public function getPDO(): PDO
     {
         return $this->pgPDO;
     }
     private function __construct()
     {
-        global $arParameter;
-        $arParameter = array();
-        if(file_exists("DB/ConnectParameter.php")){
-            require_once("DB/ConnectParameter.php");
+        global $env;
+        $env = array();
+        if(file_exists($_SERVER['DOCUMENT_ROOT']."/.env")){
+            $env = parse_ini_file($_SERVER['DOCUMENT_ROOT'].'/.env');
+        } else {
+            throw new Exception('ENV file not found');
         }
 
         $this->pgPDO = new PDO(
-            "pgsql:host={$arParameter["host"]};port={$arParameter["port"]};dbname={$arParameter["dbname"]}",
-            $arParameter["user"],
-            $arParameter["password"],
+            "pgsql:host={$env["HOST"]};port={$env["PORT"]};dbname={$env["DBNAME"]}",
+            $env["USER"],
+            $env["PASSWORD"],
             []
         );
     }
